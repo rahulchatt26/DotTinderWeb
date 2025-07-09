149482de88b169e697cbd919048d9f6119bd2e32
Rahul Chatterjee <chatterjeerahul2604@gmail.com>
2025-07-09 21:11:40 +0530

    routing configuration

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import "./App.css";
-
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Body from "./components/Body";
+import Login from "./components/Login";
+import Profile from "./components/Profile";
 function App() {
   return (
-    <>
-      <h1 className="text-3xl font-bold underline">Dot Tinder</h1>
-    </>
+    <BrowserRouter basename="/">
+      <Routes>
+        <Route path="/" element={<Body />}>
+          <Route path="/login" element={<Login />} />
+          <Route path="/profile" element={<Profile />} />
+        </Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
