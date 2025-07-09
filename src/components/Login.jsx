9bc19e074431ec16b3fe2c3de048257e34080fd5
Rahul Chatterjee <chatterjeerahul2604@gmail.com>
2025-07-09 23:27:29 +0530

    axios configuratiofor api calls

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -1,5 +1,63 @@
+import axios from "axios";
+import { useState } from "react";
+
 const Login = function () {
-  return <div>Login</div>;
+  const [emailId, setEmailId] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleLogin = async () => {
+    try {
+      const res = await axios.post(
+        "http://localhost:2604/login",
+        {
+          emailId,
+          password,
+        },
+        { withCredentials: true }
+      );
+
+      console.log(res);
+    } catch (err) {
+      console.log(err);
+    }
+  };
+
+  return (
+    <div className="flex justify-center my-5">
+      <div className="card bg-base-300 w-96 shadow-sm">
+        <div className="card-body">
+          <h2 className="card-title justify-center">Login</h2>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Email {emailId}</legend>
+            <input
+              type="text"
+              className="input"
+              value={emailId}
+              onChange={(e) => {
+                setEmailId(e.target.value);
+              }}
+            />
+          </fieldset>
+          <fieldset className="fieldset">
+            <legend className="fieldset-legend">Password</legend>
+            <input
+              type="text"
+              className="input"
+              value={password}
+              onChange={(e) => {
+                setPassword(e.target.value);
+              }}
+            />
+          </fieldset>
+          <div className="card-actions justify-center">
+            <button className="btn btn-primary" onClick={handleLogin}>
+              Login
+            </button>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Login;
